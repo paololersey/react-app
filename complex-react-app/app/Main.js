a2e45576892f2272ada60555d6fa358745a70817
paolo.spadoni <Paolo>
2020-10-31 09:05:57 +0100

    section-9

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,125 +1,111 @@
 import React, { useState, useReducer, useEffect } from "react"
-import { useImmerReducer } from "use-immer"
 import ReactDOM from "react-dom"
+import { useImmerReducer } from "use-immer"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { CSSTransition } from "react-transition-group"
 import Axios from "axios"
-
 Axios.defaults.baseURL = "http://localhost:8080"
 
-// Components
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
+
+// My Components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
-import Home from "./components/Home"
-import Profile from "./components/Profile"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from "./components/FlashMessages"
-
-// Contexts
-//import ExampleContext from "./ExampleContext"
-import StateContext from "./StateContext"
-import DispatchContext from "./DispatchContext"
+import Profile from "./components/Profile"
 import EditPost from "./components/EditPost"
+import NotFound from "./components/NotFound"
+import Search from "./components/Search"
 
 function Main() {
   const initialState = {
-    loggedIn: Boolean(localStorage.getItem("complexAppToken")),
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
     flashMessages: [],
     user: {
-      token: localStorage.getItem("complexAppToken"),
-      username: localStorage.getItem("complexAppUsername"),
-      avatar: localStorage.getItem("complexAppAvatar"),
+      token: localStorage.getItem("complexappToken"),
+      username: localStorage.getItem("complexappUsername"),
+      avatar: localStorage.getItem("complexappAvatar")
     },
+    isSearchOpen: false
   }
-  // useReducer will call us a function with an initial state for every property. It returns then 2 things,
-  // the new state and the dispatch. It is powerful since we can handle a combination of state and action we
-  // can handle in a single point
-  //
-  // const [state, dispatch] = useReducer(ourReducer, initialState)
-  // immer gives us a copy of state, call draft
-  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
-  // anytime loggedIn changes, the  function body will run
-  useEffect(() => {
-    if (state.loggedIn) {
-      // save data to localStorage
-      localStorage.setItem("complexAppToken", state.user.token)
-      localStorage.setItem("complexAppUsername", state.user.username)
-      localStorage.setItem("complexAppAvatar", state.user.avatar)
-    } else {
-      localStorage.removeItem("complexAppToken")
-      localStorage.removeItem("complexAppUsername")
-      localStorage.removeItem("complexAppAvatar")
-    }
-  }, [state.loggedIn])
-  // all logic in this function
+
   function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        // return { loggedIn: true, flashMessages: state.flashMessages } // flashMessages we don't mutate directly the state
-        // use immer with draft
         draft.loggedIn = true
         draft.user = action.data
         return
       case "logout":
-        // return { loggedIn: false, flashMessages: state.flashMessages }
-        // use immer with draft
         draft.loggedIn = false
         return
       case "flashMessage":
-        // return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) }
         draft.flashMessages.push(action.value)
+        return
+      case "openSearch":
+        draft.isSearchOpen = true
+        return
+      case "closeSearch":
+        draft.isSearchOpen = false
         return
     }
   }
 
-  // The useState with these 2 const will not be used anymore because of useReducer
-  //const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexAppToken")))
-  //const [flashMessages, setFlashMessages] = useState([])
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
-  //function addFlashMessage(msg) {
-  //  setFlashMessages((prev) => prev.concat(msg))
-  //}
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexappToken", state.user.token)
+      localStorage.setItem("complexappUsername", state.user.username)
+      localStorage.setItem("complexappAvatar", state.user.avatar)
+    } else {
+      localStorage.removeItem("complexappToken")
+      localStorage.removeItem("complexappUsername")
+      localStorage.removeItem("complexappAvatar")
+    }
+  }, [state.loggedIn])
 
   return (
-    // any child component in value will be able to access that value.
-    // This is removed
-    // <ExampleContext.Provider value={{ addFlashMessage, setLoggedIn }}>
-
-    // This leads to performance issues, we have to separate state and dispatch
-    //<ExampleContext.Provider value={{ state, dispatch }}>
-
-    // A context for state and a context for dispatch
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
         <BrowserRouter>
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
             <Route path="/" exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
-            <Route path="/profile/:username" exact>
-              <Profile />
-            </Route>
-            <Route path="/create-post" exact>
-              <CreatePost></CreatePost>
-            </Route>
             <Route path="/post/:id" exact>
-              <ViewSinglePost></ViewSinglePost>
-            </Route>
-            <Route path="/about-us" exact>
-              <About></About>
+              <ViewSinglePost />
             </Route>
             <Route path="/post/:id/edit" exact>
               <EditPost />
             </Route>
-            <Route path="/terms" exact>
-              <Terms></Terms>
+            <Route path="/create-post">
+              <CreatePost />
+            </Route>
+            <Route path="/about-us">
+              <About />
+            </Route>
+            <Route path="/terms">
+              <Terms />
+            </Route>
+            <Route>
+              <NotFound />
             </Route>
           </Switch>
+          <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
+            <Search />
+          </CSSTransition>
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
--- complex-react-app/app/components/EditPost.js
+++ complex-react-app/app/components/EditPost.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState, useContext } from "react"
 import { useImmerReducer } from "use-immer"
 import Page from "./Page"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, withRouter } from "react-router-dom"
 import Axios from "axios"
 import LoadingDotsIcon from "./LoadingDotsIcon"
 import StateContext from "../StateContext"
 import DispatchContext from "../DispatchContext"
+import NotFound from "./NotFound"
 
-function ViewSinglePost() {
+function EditPost(props) {
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
 
@@ -25,7 +26,8 @@ function ViewSinglePost() {
     isFetching: true,
     isSaving: false,
     id: useParams().id,
-    sendCount: 0
+    sendCount: 0,
+    notFound: false
   }
 
   function ourReducer(draft, action) {
@@ -66,6 +68,9 @@ function ViewSinglePost() {
           draft.body.message = "You must provide body content."
         }
         return
+      case "notFound":
+        draft.notFound = true
+        return
     }
   }
 
@@ -83,7 +88,16 @@ function ViewSinglePost() {
     async function fetchPost() {
       try {
         const response = await Axios.get(`/post/${state.id}`, { cancelToken: ourRequest.token })
-        dispatch({ type: "fetchComplete", value: response.data })
+        if (response.data) {
+          dispatch({ type: "fetchComplete", value: response.data })
+          if (appState.user.username != response.data.author.username) {
+            appDispatch({ type: "flashMessage", value: "You do not have permission to edit that post." })
+            // redirect to homepage
+            props.history.push("/")
+          }
+        } else {
+          dispatch({ type: "notFound" })
+        }
       } catch (e) {
         console.log("There was a problem or the request was cancelled.")
       }
@@ -114,6 +128,10 @@ function ViewSinglePost() {
     }
   }, [state.sendCount])
 
+  if (state.notFound) {
+    return <NotFound />
+  }
+
   if (state.isFetching)
     return (
       <Page title="...">
@@ -123,7 +141,11 @@ function ViewSinglePost() {
 
   return (
     <Page title="Edit Post">
-      <form onSubmit={submitHandler}>
+      <Link className="small font-weight-bold" to={`/post/${state.id}`}>
+        &laquo; Back to post permalink
+      </Link>
+
+      <form className="mt-3" onSubmit={submitHandler}>
         <div className="form-group">
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
@@ -148,4 +170,4 @@ function ViewSinglePost() {
   )
 }
 
-export default ViewSinglePost
+export default withRouter(EditPost)
